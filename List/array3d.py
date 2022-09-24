0ee12de203916ea188690de15510fa642900c11b
Rakesh Chanda <113673497+rakeshchanda2520@users.noreply.github.com>
2022-09-24 12:22:03 +0530

    Delete List directory

--- List/array3d.py
+++
@@ -1,8 +0,0 @@
-# Write a Python program to generate a 3*4*6 3D array whose each element is *
-
-list=[]
-for i in range(3):
-    for j in range(4):
-        for k in range(6):
-            print("*",end="")
-        print()
--- List/listemptyornot.py
+++
@@ -1,7 +0,0 @@
-# Python program to check a list is empty or not.
-
-list1=[1]
-if len(list1)==0:
-    print("empty")
-else:
-    print("not empty")
